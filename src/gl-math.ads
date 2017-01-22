b87aa79ec623cab1c6f481d2886ce6a4e59ad783
Johan <johaa1993@gmail.com>
2017-01-22 02:33:20 +0100

    test

--- src/gl-math.ads
+++ src/gl-math.ads
@@ -1,22 +1,28 @@
+with Ada.Numerics.Generic_Elementary_Functions;
 with GL.C;
 
 package GL.Math is
 
    use GL.C;
 
+   package Elementary_Functions is new Ada.Numerics.Generic_Elementary_Functions (GLfloat);
+
+   subtype Dimension is Integer;
    subtype Dimension_1 is Integer range 1 .. 1;
    subtype Dimension_2 is Integer range 1 .. 2;
    subtype Dimension_3 is Integer range 1 .. 3;
    subtype Dimension_4 is Integer range 1 .. 4;
 
-   type Vector_1 is array (Dimension_1) of GLfloat;
-   type Vector_2 is array (Dimension_2) of GLfloat;
-   type Vector_3 is array (Dimension_3) of GLfloat;
-   type Vector_4 is array (Dimension_4) of GLfloat;
+   type Vector is array (Dimension range <>) of GLfloat;
+   subtype Vector_1 is Vector (Dimension_1);
+   subtype Vector_2 is Vector (Dimension_2);
+   subtype Vector_3 is Vector (Dimension_3);
+   subtype Vector_4 is Vector (Dimension_4);
 
-   type Matrix_1 is array (Dimension_1, Dimension_1) of GLfloat;
-   type Matrix_2 is array (Dimension_2, Dimension_2) of GLfloat;
-   type Matrix_3 is array (Dimension_3, Dimension_3) of GLfloat;
-   type Matrix_4 is array (Dimension_4, Dimension_4) of GLfloat;
+   type Matrix is array (Dimension range <>, Dimension range <>) of GLfloat;
+   subtype Matrix_1 is Matrix (Dimension_1, Dimension_1);
+   subtype Matrix_2 is Matrix (Dimension_2, Dimension_2);
+   subtype Matrix_3 is Matrix (Dimension_3, Dimension_3);
+   subtype Matrix_4 is Matrix (Dimension_4, Dimension_4);
 
 end;
